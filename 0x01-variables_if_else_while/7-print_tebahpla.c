998c75a5528d8678dce03f4b2884d47b8932953e
Sharon <sharoneunice1@gmail.com>
2022-03-12 07:25:08 -0800

    seventh task

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ *main - print alphabet in reverse form
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	for (i = 'z'; i >= 'a'; i--)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
