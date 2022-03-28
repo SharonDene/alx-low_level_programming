8720d66340636ea34de6c4d30bd15aa71a1b266e
Sharon <sharoneunice1@gmail.com>
2022-03-28 16:23:08 -0700

    hope it's working

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *_memcpy - copies memory area
+ *@dest: memory area 2
+ *@src: memory area 1
+ *@n: bytes
+ *Return: a pointer to dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		*(dest + 1) = *(src + 1);
+
+	return (dest);
+}
---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ *_strspn - gets the length of a prefix substring
+ *@s:  initial segment
+ *@accept: bytes
+ *Return: the number of bytes in the initial segment
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int n;
+}
