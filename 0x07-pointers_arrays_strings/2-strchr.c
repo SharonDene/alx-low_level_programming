4fc1235841a3e53198e60560b398fa5ec4655d4d
Sharon <sharoneunice1@gmail.com>
2022-03-29 14:38:08 -0700

    trying

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -4,14 +4,14 @@
  *_strchr - locates a character in a string
  *@c: charcater to locate
  *@s: string
- *Rreturn: a pointer to the first occurrence of the character
+ *Return: a pointer to the first occurrence of the character
  */
 
 char *_strchr(char *s, char c)
 {
 	unsigned int i = 0;
 
-	for (; *(s + 1) != '\0'; i++)
+	for (; *(s + i) != '\0'; i++)
 		if (*(s + i) == c)
 			return (s + i);
 
---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ *_strstr - locates a substring
+ *@needle: substring
+ *@haystack: string
+ *Return: a pointer to the beginning of the located substring
+ *or NULL if the substring is not found
+ */
+
+char *_strstr(char *haystack, char *needle)
